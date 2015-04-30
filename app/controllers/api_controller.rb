3ace29c4229178332efdff27e7b298f7b2353a57
Hii <yonglian146@gmail.com>
2015-04-30 15:17:53 +0800

    testing parsing html android

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -89,7 +89,7 @@ class ApiController < ApplicationController
             while !announcement_generic_path.xpath("div[#{announcement_number}]/font").empty? do
               announcement = week.announcements.build
               announcement.title = announcement_generic_path.xpath("div[#{announcement_number}]/font").inner_text.delete("\r").delete("\t")
-              announcement.contents = announcement_generic_path.xpath("div[#{announcement_number}]").children[7..-1].text.delete("\r\t")
+              announcement.contents = announcement_generic_path.xpath("div[#{announcement_number}]").children[7..-1]
               announcement.author = announcement_generic_path.xpath("div[#{announcement_number}]/div[1]/i[1]").text.delete("\r").delete("\n").delete("\t").split("  ;   ").first[3..-1]
               announcement.posted_date = announcement_generic_path.xpath("div[#{announcement_number}]/div[1]/i[1]").text.delete("\r").delete("\n").delete("\t").split("               ").last
               announcement_number = announcement_number + 1
@@ -321,7 +321,7 @@ class ApiController < ApplicationController
         while !announcement_generic_path.xpath("div[#{announcement_number}]/font").empty? do
           announcement = week.announcements.build
           announcement.title = announcement_generic_path.xpath("div[#{announcement_number}]/font").inner_text.delete("\r").delete("\t")
-          announcement.contents = announcement_generic_path.xpath("div[#{announcement_number}]").children[7..-1].text.delete("\r\t")
+          announcement.contents = announcement_generic_path.xpath("div[#{announcement_number}]").children[7..-1]#.text.delete("\r\t")
           announcement.author = announcement_generic_path.xpath("div[#{announcement_number}]/div[1]/i[1]").text.delete("\r").delete("\n").delete("\t").split("  ;   ").first[3..-1]
           announcement.posted_date = announcement_generic_path.xpath("div[#{announcement_number}]/div[1]/i[1]").text.delete("\r").delete("\n").delete("\t").split("               ").last
           announcement_number = announcement_number + 1
