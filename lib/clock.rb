9914b6d2a7dcd4c8a77daf7a24f4cddd65bfb84e
Hii <yonglian146@gmail.com>
2015-04-30 12:04:35 +0800

    Updated clock

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,5 +1,8 @@
+require File.expand_path('../../config/boot',        __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 require 'clockwork'
 
 include Clockwork
 
-every(1.minute, Bulletin.update_bulletin) { Sidekiq::Client.enqueue(MyWorker) }
+every(1.day, 'Run my worker daily', at: '04:30', tz: 'UTC') { Sidekiq::Client.enqueue(MyWorker) }
+every(7.day, 'Send weekly reports', at: '02:00', tz: 'UTC') { Sidekiq::Client.enqueue(ReportWorker) }
