b00894821bacc04452be367176a9330a20a07cb5
Hii <yonglian146@gmail.com>
2015-05-04 13:06:39 +0800

    Updated parser

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -277,7 +277,9 @@ class ApiController < ApplicationController
         while !announcement_generic_path.xpath("div[#{announcement_number}]/font").empty? do
           announcement = week.announcements.build
           announcement.title = announcement_generic_path.xpath("div[#{announcement_number}]/font").inner_text.delete("\r").delete("\t")
-          announcement.contents = announcement_generic_path.xpath("div[#{announcement_number}]").children[7..-1].text.delete("\r\t")
+          contents = announcement_generic_path.xpath("div[#{announcement_number}]").children[7..-1]
+          sanitized_contents = Sanitize.clean(contents, :remove_contents => ['script', 'style'])
+          announcement.contents = sanitized_contents.delete("\r\t")
           announcement.author = announcement_generic_path.xpath("div[#{announcement_number}]/div[1]/i[1]").text.delete("\r").delete("\n").delete("\t").split("  ;   ").first[3..-1]
           announcement.posted_date = announcement_generic_path.xpath("div[#{announcement_number}]/div[1]/i[1]").text.delete("\r").delete("\n").delete("\t").split("               ").last
 
