be4e57c7f303cd6a214d2c88b68006f656623224
Yong Lian Hii <Hii@Yongs-MacBook-Pro.local>
2015-08-01 22:41:25 +0800

    Updated update_bulletin

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,31 +2,7 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_filter :set_agent
   def update_bulletin
-    page = @agent.get("https://online.mmu.edu.my/index.php")
-    form = page.form
-    bulletins = []
-    form.form_loginUsername =  ENV['STUDENT_ID']
-    form.form_loginPassword = ENV['PORTAL_PASSWORD']
-    page = @agent.submit(form)
-    page = @agent.get("https://online.mmu.edu.my/bulletin.php")
-    bulletin_number = 1
-    while !page.parser.xpath("//*[@id='tabs-1']/div[#{bulletin_number}]").empty? and bulletin_number <= 20
-      url = page.parser.xpath("//*[@id='tabs-1']/div[#{bulletin_number}]/p/a/@href").text
-      unless (Bulletin.find_by_url(url))
-        print "EXECUTING " + bulletin_number.to_s + "\n"
-        bulletin_post = page.parser.xpath("//*[@id='tabs-1']/div[#{bulletin_number}]")
-        bulletin = Bulletin.new
-        bulletin.title = bulletin_post.xpath("p/a[1]").text
-        bulletin_details = bulletin_post.xpath("div/div/text()").text.split("\r\n        ").delete_if(&:empty?)
-        bulletin.posted_on = Time.parse(bulletin_details[0].split(" ")[2..5].join(" "))
-        bulletin.url = page.parser.xpath("//*[@id='tabs-1']/div[#{bulletin_number}]/p/a/@href").text
-        bulletin.expired_on = Time.parse(bulletin_details[1].split(" : ")[1])
-        bulletin.author = bulletin_details[2].split(" : ")[1].delete("\t")
-        bulletin.contents = bulletin_post.xpath("div/div/div").text.delete("\t").delete("\r")
-        bulletin.save
-      end
-      bulletin_number = bulletin_number + 1
-    end
+    Bulletin.update_bulletin
     render json: JSON.pretty_generate( Bulletin.order(posted_on: :desc,url: :desc).limit(20).as_json)
   end
 
